3daed0e16d507d72dd162ede61298fdeff57358c
lywworking <xue_2013@sina.com>
2018-05-24 23:18:02 +0800

    第一次提交

---
+++ Commons Tools/src/main/java/org/poem/collection/CollectionUtils.java
@@ -0,0 +1,12 @@
+package org.poem.collection;
+
+import java.util.Map; /**
+ * commons utils
+ */
+public final class CollectionUtils extends org.apache.commons.collections.CollectionUtils {
+
+
+    public static Boolean isNotEmpty(Map<String, Object> loaderClass) {
+        return loaderClass != null && !loaderClass.isEmpty();
+    }
+}
