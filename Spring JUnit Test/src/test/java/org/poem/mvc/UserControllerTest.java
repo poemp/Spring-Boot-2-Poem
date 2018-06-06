4e75666db91bb1ca43882f9e93c1de840d85639b
lywworking <xue_2013@sina.com>
2018-06-06 18:39:26 +0800

    Update UserControllerTest.java

--- Spring JUnit Test/src/test/java/org/poem/mvc/UserControllerTest.java
+++ Spring JUnit Test/src/test/java/org/poem/mvc/UserControllerTest.java
@@ -5,17 +5,19 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.BDDMockito;
 import org.mockito.MockitoAnnotations;
+import org.poem.json.JsonBean;
 import org.poem.user.UserController;
 import org.poem.user.UserService;
 import org.poem.user.vo.UserVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(UserController.class)
@@ -37,10 +39,15 @@ public class UserControllerTest {
         String userId = "10";
         String expectedCredit = "100";
         BDDMockito.given(this.userService.getById(userId)).willReturn(new UserVo(expectedCredit,"BDDMockito"));
+        String reseult = new JsonBean(new UserVo(expectedCredit, "name")).toString();
         mockMvc
                 .perform(MockMvcRequestBuilders.get("/api/v1.0/user/{id}",userId)
                         .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
                         .accept(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.content().string(reseult));
+                .andExpect(status().isOk()) //200
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(model().attributeExists("name"))
+                .andExpect(model().attribute("name","name"))
+                .andExpect(content().string(reseult));
     }
 }
